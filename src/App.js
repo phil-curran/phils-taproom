d56188b4580eb982bf2aa8c52e982c98c41bd9ce
Phil Curran <pecurran@hotmail.com>
2022-06-03 16:11:20 -0700

    got KegList to output KegCard with .map and inventory.js array.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
 import "../node_modules/bulma/css/bulma.min.css";
 import "./App.css";
+import About from "./Components/About";
+import AddKeg from "./Components/AddKeg";
+import EditKeg from "./Components/EditKeg";
+import KegDetail from "./Components/KegDetail";
+import KegList from "./Components/KegList";
+import Inventory from "./inventory";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <hr></hr>
+      <About />
+      <hr></hr>
+      <AddKeg />
+      <hr></hr>
+      <EditKeg />
+      <hr></hr>
+      <KegDetail />
+      <hr></hr>
+      <KegList Inventory={Inventory} />
     </div>
   );
 }
---
+++ src/inventory.js
@@ -0,0 +1,36 @@
+import uuid from "react-uuid";
+
+const Inventory = [
+  {
+    id: uuid(),
+    name: "Sammish Haze",
+    style: "NEIPA",
+    brewery: "Black Raven Brewing",
+    description: "Lorem ipsum dolor si amet blah blah blah...",
+    price: 5,
+    abv: 5.5,
+    ibu: 65,
+  },
+  {
+    id: uuid(),
+    name: "The Seer",
+    style: "Table Beer",
+    brewery: "Holy Mountain Brewing",
+    description: "Lorem ipsum dolor si amet blah blah blah...",
+    price: 7,
+    abv: 6.5,
+    ibu: 30,
+  },
+  {
+    id: uuid(),
+    name: "Strawberry Gose",
+    style: "Geuze",
+    brewery: "Ravenna Brewing Co.",
+    description: "Lorem ipsum dolor si amet blah blah blah...",
+    price: 5,
+    abv: 7.5,
+    ibu: 75,
+  },
+];
+
+export default Inventory;
