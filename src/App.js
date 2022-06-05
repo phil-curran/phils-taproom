53c75b771398cf0e15a469078abe6ad8a1634df9
Phil Curran <pecurran@hotmail.com>
2022-06-04 17:38:18 -0700

    Revert "updated name of Header to Navbar, got links working in Navbar for About and Home / KegList."
    This reverts commit 2c248cefcdf79979ef85b74245dae8d3a63bad7b.
    
    rolling back to prior working version.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import "../node_modules/bulma/css/bulma.css";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React from "react";
 import { useState } from "react";
-import Navbar from "./components/Navbar";
+import About from "./components/About";
 import KegList from "./components/KegList";
+import Inventory from "./data/Inventory";
+import Header from "./components/Header";
 import AddKeg from "./components/AddKeg";
-import KegDetail from "./components/KegDetail";
 
-const App = () => {
-  const [kegList, setKegList] = useState([{ name: "Phil", age: 43 }]);
-  const [selectedKeg, setSelectedKeg] = useState(null);
-
+function App() {
+  const [inventory, setInventory] = useState();
   return (
-    <Router>
-      <Navbar />
-      <div className="content">
-        <Switch>
-          <Route exact path="/" component={KegList} />
-          <Route exact path="/keglist" component={KegList} kegList={kegList} />
-          <Route exact path="/addkeg" component={AddKeg} />
-          <Route exact path="/kegdetail" component={KegDetail} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <KegList inventory={Inventory} />
+      <AddKeg inventory={Inventory} setInventory={setInventory} />
+    </div>
   );
-};
+}
 
 export default App;
